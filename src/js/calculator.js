84e7922a600854ae19ae03b0f497f35778d5da77
Sérgio Toledo <contato@sergiotoledo.com.br>
2018-09-04 16:21:39 -0300

    Refactor

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,24 +1,39 @@
 var Calculator = {
-  add: function(num1, num2) {
-    if (isNaN(num1) || isNaN(num2)) { return 0; }
+  ADDITION: '+',
+  SUBTRACTION: '-',
+  DIVISION: '/',
+  MULTIPLICATION: '*',
 
-    return parseFloat(num1) + parseFloat(num2);
+  add: function(num1, num2) {
+    return Calculator.calculate(num1, num2, Calculator.ADDITION)
   },
   subtract: function(num1, num2) {
-    if (isNaN(num1) || isNaN(num2)) { return 0; }
-    
-    return num1 - num2;
+    return Calculator.calculate(num1, num2, Calculator.SUBTRACTION)
   },
   divide: function(num1, num2) {
     if (num2 == 0) { return 'Error, division by zero'; }
-    if (isNaN(num1) || isNaN(num2)) { return 0; }
-    
-    return num1 / num2
+    return Calculator.calculate(num1, num2, Calculator.DIVISION)
   },
   multiply: function(num1, num2) {
-    if (isNaN(num1) || isNaN(num2)) { return 0; }
+    return Calculator.calculate(num1, num2, Calculator.MULTIPLICATION)
+  },
+  
+  calculate: function(num1, num2, operator) {
+    if (isNaN(num1) || isNaN(num2)) { return 0 }
 
-    return num1 * num2;
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    switch(operator) {
+      case Calculator.ADDITION:
+        return result = num1 + num2;
+      case Calculator.SUBTRACTION:
+        return result = num1 - num2;
+      case Calculator.DIVISION:
+        return result = num1 / num2;
+      case Calculator.MULTIPLICATION:
+        return result = num1 * num2;
+    }
   }
 };
 
